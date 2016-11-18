011c8e3d4a4334606861d5b2467f23c6553ba7cc
Mike Heffner <mike@librato.com>
2016-11-18 10:28:24 -0500

    Perform pre-check if we are on EC2.

--- src/daemon/utils_ec2_meta.c
+++ src/daemon/utils_ec2_meta.c
@@ -24,17 +24,15 @@ struct ec2_meta {
 	struct ec2_meta_field *az;
 };
 
-
 static size_t ec2_meta_callback(void *buf, size_t size,
     size_t nmemb, void *user_data)
 {
-	struct ec2_meta_field *field = (struct ec2_meta_field *)user_data;
-
-	if (field == NULL) {
-		return 0;
-	}
-
 	size_t len = size * nmemb;
+
+	struct ec2_meta_field *field = (struct ec2_meta_field *)user_data;
+	if (field == NULL) {
+		return len;
+	}
 
 	if (field->filled + len >= sizeof(field->buf)) {
 		return (0);
@@ -47,25 +45,48 @@ static size_t ec2_meta_callback(void *buf, size_t size,
 	return len;
 }
 
-static struct ec2_meta_field *ec2_meta_get_field(const char *path)
+static int ec2_meta_is_success(CURL *curl, int status)
+{
+	long http_code = 0;
+
+	curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+	return (http_code == 200 && status != CURLE_ABORTED_BY_CALLBACK) ? 0 : -1;
+}
+
+static int ec2_meta_check_ec2(CURL *curl)
+{
+	char url[1024];
+
+	snprintf(url, sizeof(url), BASE_EC2_URL);
+
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ec2_meta_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, COLLECTD_USERAGENT);
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0L);
+	curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 0L);
+
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, CURL_TIMEOUT_MS);
+
+	int status = curl_easy_perform(curl);
+	if (status != CURLE_OK) {
+		return -1;
+	}
+
+	return ec2_meta_is_success(curl, status);
+}
+
+static struct ec2_meta_field *ec2_meta_get_field(CURL *curl, const char *path)
 {
 	char url[1024];
 	char errbuf[CURL_ERROR_SIZE];
 
 	snprintf(url, sizeof(url), "%s%s", BASE_EC2_URL, path);
 
-	CURL *curl;
-
 	struct ec2_meta_field *field = calloc(1, sizeof(*field));
 	if (field == NULL) {
 		ERROR("ec2_meta: failed to allocate field");
-		return NULL;
-	}
-
-	curl = curl_easy_init();
-	if (curl == NULL) {
-		ERROR ("ec2_meta: curl_easy_init failed");
-		free(field);
 		return NULL;
 	}
 
@@ -84,56 +105,76 @@ static struct ec2_meta_field *ec2_meta_get_field(const char *path)
 	if (status != CURLE_OK) {
 		ERROR("ec2_meta: curl_easy_perform failed with status %d: %s",
 		    status, errbuf);
-		curl_easy_cleanup(curl);
 		free(field);
 		return NULL;
 	}
 
-	curl_easy_cleanup(curl);
+	if (ec2_meta_is_success(curl, status) != 0) {
+		free(field);
+		return NULL;
+	}
 
 	return field;
 }
 
 static struct ec2_meta *ec2_meta_get_fields()
 {
-	struct ec2_meta *ec2;
+	struct ec2_meta *ec2 = NULL;
+
+	CURL *curl = curl_easy_init();
+	if (curl == NULL) {
+		ERROR("ec2_meta: curl_easy_init failed");
+		return NULL;
+	}
+
+	if (ec2_meta_check_ec2(curl) != 0) {
+		curl_easy_cleanup(curl);
+		return NULL;
+	}
 
 	ec2 = calloc(1, sizeof(*ec2));
 	if (ec2 == NULL) {
 		ERROR("ec2_meta: failed to allocate memory");
-		return NULL;
+		goto failed;
 	}
 
-	ec2->instance_id = ec2_meta_get_field("instance-id");
+	ec2->instance_id = ec2_meta_get_field(curl, "instance-id");
 	if (ec2->instance_id == NULL) {
 		ERROR("ec2_meta: failed to get field instance_id");
 		goto failed;
 	}
 
-	ec2->instance_type = ec2_meta_get_field("instance-type");
+	ec2->instance_type = ec2_meta_get_field(curl, "instance-type");
 	if (ec2->instance_type == NULL) {
 		ERROR("ec2_meta: failed to get field instance_type");
 		goto failed;
 	}
 
-	ec2->az = ec2_meta_get_field("placement/availability-zone");
+	ec2->az = ec2_meta_get_field(curl, "placement/availability-zone");
 	if (ec2->az == NULL) {
 		ERROR("ec2_meta: failed to get field availability-zone");
 		goto failed;
 	}
 
+	curl_easy_cleanup(curl);
+
 	return ec2;
 
   failed:
 
-	if (ec2->az != NULL)
-		free(ec2->az);
-	if (ec2->instance_type != NULL)
-		free(ec2->instance_type);
-	if (ec2->instance_id != NULL)
-		free(ec2->instance_id);
+	if (ec2 != NULL) {
+		if (ec2->az != NULL)
+			free(ec2->az);
+		if (ec2->instance_type != NULL)
+			free(ec2->instance_type);
+		if (ec2->instance_id != NULL)
+			free(ec2->instance_id);
 
-	free(ec2);
+		free(ec2);
+	}
+
+	curl_easy_cleanup(curl);
+
 	return NULL;
 }
 
